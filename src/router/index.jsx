1c8e278147d98a540f5243cddc7bdabc035e5b19
Wannzh <alwanfdhlrhmn@gmail.com>
2025-12-08 17:23:34 +0700

    Add Setup router Login & Regis

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,19 +4,25 @@ import HomePage from "@/pages/HomePage";
 import NotFound from "@/pages/NotFound";
 
 import AppLayout from "@/components/layout/AppLayout";
+import LoginPage from "@/features/auth/pages/LoginPage";
+import RegisterPage from "@/features/auth/pages/RegisterPage";
 
 export default function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Layout wrapper */}
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Layout wrapper */}
+                <Route element={<AppLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                </Route>
 
-        {/* Not Found */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+                {/* Auth */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+
+                {/* Not Found */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
