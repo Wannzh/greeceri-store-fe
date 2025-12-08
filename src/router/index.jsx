bbb8a9efd844b585d35d5186ea6c061cca09db1c
Wannzh <alwanfdhlrhmn@gmail.com>
2025-12-08 17:27:27 +0700

    Add Setup router Product

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,6 +6,8 @@ import NotFound from "@/pages/NotFound";
 import AppLayout from "@/components/layout/AppLayout";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import RegisterPage from "@/features/auth/pages/RegisterPage";
+import ProductListPage from "@/features/product/pages/ProductListPage";
+import ProductDetailPage from "@/features/product/pages/ProductDetailPage";
 
 export default function AppRouter() {
     return (
@@ -19,6 +21,10 @@ export default function AppRouter() {
                 {/* Auth */}
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+
+                {/* Product */}
+                <Route path="/products" element={<ProductListPage />} />
+                <Route path="/products/:id" element={<ProductDetailPage />} />
 
                 {/* Not Found */}
                 <Route path="*" element={<NotFound />} />
